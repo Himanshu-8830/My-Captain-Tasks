6a2c20ba6b58c2e67e11dcf24f7ce9d531b46732
Himanshu-8830 <82021925+Himanshu-8830@users.noreply.github.com>
2021-06-18 21:31:54 +0530

    Add files via upload

---
+++ my captain task 1.py
@@ -0,0 +1,9 @@
+r=float(input("Input the radius of the circle:"))
+area=3.14*r*r
+print("The area of the circle with radius ",r,"is :",area)
+
+
+
+f=input("Input the filename:")
+fn=f.split(".")
+print("The extension of the file is : "+fn[-1])
